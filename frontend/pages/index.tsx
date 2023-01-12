761132e493318360f94ad9a9b4c9196e1605118b
Joe Joe <b.8080ra@gmail.com>
2023-01-12 18:50:48 -0500

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
     <p className='text-white mx-auto mt-[10vh] text-center text-2xl sm:text-5xl sm:w-[70vw] font-extralight'>Welcome to EarthCraft, a 1 to 200 scale Minecraft Earth map server where players can create or join nations. Our server is the perfect place for players who want to explore and create on a realistic, Earth-like map. You&apos;ll have the opportunity to work with other players to build and maintain your own nation, fight in wars and expand your territorial borders.</p>
     </div>
     <p className='text-white mt-[30vh] text-center text-3xl sm:text-5xl font-extralight'>Map Scale</p>
-    <p className='text-white mt-[5vh] text-center text-5xl md:text-[10rem] leading-none mb-[5vh]' style={{fontFamily:"'Minecraftia', sans-serif"}}>1:200</p>
+    <p className='text-white mt-[5vh] text-center text-7xl md:text-[10rem] leading-none mb-[5vh]' style={{fontFamily:"'Minecraftia', sans-serif"}}>1:200</p>
     <p className='text-white mb-[20vh] text-center text-2xl sm:text-4xl font-light'>The <span className='font-bold'>largest</span> map size currently in the geopolitical community.</p>
 
     <div className='flex bg-mcbg1 bg-fixed bg-no-repeat bg-cover h-[40vh]'>
