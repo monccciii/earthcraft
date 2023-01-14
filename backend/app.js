c7a9b52cb21cd5da9b30a8f1e2c530ce437cda1d
Joe Joe <b.8080ra@gmail.com>
2023-01-14 08:37:52 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/', (req, res) => {
+app.get('/getnations', (req, res) => {
+    res.send('Hello World!')
+  })  
+  app.get('/getbusinesses', (req, res) => {
     res.send('Hello World!')
   })  
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
